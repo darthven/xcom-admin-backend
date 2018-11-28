b9db1058bd40ebc205c70b3d02c4e8baa92ae0be
darthven <kostharl@gmail.com>
2018-11-28 16:46:44 +0200

    Some fixes were made

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'xcom-admin',
+            script: './lib/index.js',
+            watch: false,
+            "log_date_format": "DD-MM-YYYY HH:mm:ss.SSS"
+        }
+    ]
+};
