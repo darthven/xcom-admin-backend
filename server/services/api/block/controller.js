1e14c7720670acd1686162fd24acfec8422ab78c
darthven <kostharl@gmail.com>
2018-11-29 17:35:24 +0200

    New endpoint for blocks were added

--- server/services/api/block/controller.js
+++ server/services/api/block/controller.js
@@ -1,4 +1,7 @@
+import request from 'request-promise-native'
+
 import Block from './../../../db/models/block'
+import { XCOM_URL } from '../../../config/env.config';
 
 async function getBlock (ctx) {
     const { id } = ctx.params
@@ -13,6 +16,16 @@ async function getBlock (ctx) {
 
 async function getBlocks (ctx) {
     ctx.response.body = (await Block.find()).map(bl => bl._doc)
+    ctx.status = 200   
+}
+
+async function getBlocksByRegionWithProducts (ctx) {
+    const result = (await Block.find({ regionId: ctx.query.regionId })).map(bl => bl._doc)
+    for (const [index, doc] of result.entries()) {
+        const products = await request.get(`${XCOM_URL}/goods/by/ids/?ids=${doc.productIds.toString()}&region=${ctx.query.regionId}`)
+        result[index].products = JSON.parse(products)
+    }
+    ctx.response.body = result
     ctx.status = 200   
 }
 
@@ -54,6 +67,7 @@ async function deleteBlock (ctx) {
 export default {
     getBlock,
     getBlocks,
+    getBlocksByRegionWithProducts,
     createBlock,
     updateBlock,
     deleteBlock
