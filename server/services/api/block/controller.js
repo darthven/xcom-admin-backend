1090491e0ef332b3a2512e0624bc5dd6957ac22d
darthven <kostharl@gmail.com>
2018-12-12 19:16:43 +0200

    Some fixes

--- server/services/api/block/controller.js
+++ server/services/api/block/controller.js
@@ -1,62 +1,66 @@
+import Block from "./../../../db/models/block";
 
-import Block from './../../../db/models/block'
+async function getBlock(ctx) {
+  const { id } = ctx.params;
+  const block = await Block.findOne({ _id: id });
+  if (block) {
+    ctx.response.body = block._doc;
+    ctx.status = 200;
+  } else {
+    ctx.status = 404;
+  }
+}
 
-async function getBlock (ctx) {
-    const { id } = ctx.params
-    const block = await Block.findOne({ _id: id }) 
-    if (block) {
-        ctx.response.body = block._doc
-        ctx.status = 200
-    } else {
-        ctx.status = 404
+async function getBlocks(ctx) {
+  const query = ctx.query.regionId
+    ? { regionId: { $in: [ctx.query.regionId, 0] } }
+    : {};
+  ctx.response.body = (await Block.find(query)).map(bl => bl._doc);
+  ctx.status = 200;
+}
+
+async function createBlock(ctx) {
+  const block = new Block(ctx.request.body);
+  await block.save();
+  ctx.body = block;
+  ctx.status = 200;
+}
+
+async function updateBlock(ctx) {
+  const { id } = ctx.params;
+  const { title, regionId, storeId, productIds, active } = ctx.request.body;
+  await Block.updateOne(
+    { _id: id },
+    {
+      title,
+      regionId,
+      storeId,
+      productIds,
+      active
     }
+  );
+  ctx.body = new Block({
+    _id: id,
+    title,
+    regionId,
+    storeId,
+    productIds,
+    active
+  });
+  ctx.status = 200;
 }
 
-async function getBlocks (ctx) {
-    const query = ctx.query.regionId ? { regionId: ctx.query.regionId } : {}
-    ctx.response.body = (await Block.find(query)).map(bl => bl._doc)
-    ctx.status = 200   
-}
-
-async function createBlock (ctx) {
-    const block = new Block(ctx.request.body)
-    await block.save()
-    ctx.body = block
-    ctx.status = 200 
-}
-
-async function updateBlock (ctx) {
-    const { id } = ctx.params
-    const { title, regionId, storeId, productIds, active } = ctx.request.body
-    await Block.updateOne({ _id: id }, {
-        title,
-        regionId,
-        storeId,
-        productIds,
-        active
-    })
-    ctx.body = new Block({
-        _id: id, 
-        title,
-        regionId,
-        storeId,
-        productIds,
-        active
-    })
-    ctx.status = 200    
-}
-
-async function deleteBlock (ctx) {
-    const { id } = ctx.params
-    await Block.deleteOne({ _id: id })
-    ctx.body = id
-    ctx.status = 200        
+async function deleteBlock(ctx) {
+  const { id } = ctx.params;
+  await Block.deleteOne({ _id: id });
+  ctx.body = id;
+  ctx.status = 200;
 }
 
 export default {
-    getBlock,
-    getBlocks,
-    createBlock,
-    updateBlock,
-    deleteBlock
-}
+  getBlock,
+  getBlocks,
+  createBlock,
+  updateBlock,
+  deleteBlock
+};
